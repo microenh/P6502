2f086eb686ac32a2d858134f30c8e8e31133f48b
Mark Erbaugh <mark.election@gmail.com>
2025-02-18 10:51:50 -0500

    Initial Commit

---
+++ P6502/P6502App.swift
@@ -0,0 +1,17 @@
+//
+//  P6502App.swift
+//  P6502
+//
+//  Created by Mark Erbaugh on 2/18/25.
+//
+
+import SwiftUI
+
+@main
+struct P6502App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
